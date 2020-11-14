2bcd17d497aa13c02a0c7bd7c9e2bc248e9f7eef
Kenneth Carter <kxccarter@gmail.com>
2020-11-14 16:23:01 -0600

    successfuly impliments logic to call to jService API, sort results by clue value, then randomly select a single clue for each clue value level.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import jService from '../../api/jService';
+import filterCluesWithValues from '../../logic/getCategoryClues';
 
 import generateCategoryIdSet from '../../logic/logic';
 
 function App() {
+  useEffect(() => {
+    getClues();
+  }, []);
+
+  const getClues = async () => {
+    const { data } = await jService.get('/', {
+      params: {
+        id: ids[0],
+      },
+    });
+
+    return console.log(filterCluesWithValues(data.clues, data.title));
+  };
+
   const ids = generateCategoryIdSet();
+
   const catergoryIds = ids.map((id) => {
     return <p key={id}>{id}</p>;
   });
---
+++ src/logic/getCategoryClues.js
@@ -0,0 +1,75 @@
+import jService from '../api/jService';
+import generateCategoryIdSet from './logic';
+
+const ids = generateCategoryIdSet();
+
+// const getCategory = async () => {
+//   const { data } = await jService.get('/', {
+//     params: {
+//       id: ids[0],
+//     },
+//   });
+
+//   return filterCluesWithValues(data.clues, data.title);
+// };
+
+const filterCluesWithValues = (clues, category) => {
+  const validClues = clues.filter((clue) => {
+    return clue.value;
+  });
+
+  return sortCluesByValue(validClues, category);
+};
+
+const sortCluesByValue = (clues, category) => {
+  const sortedClues = {
+    100: [],
+    200: [],
+    300: [],
+    400: [],
+    500: [],
+    600: [],
+  };
+
+  for (let clue of clues) {
+    switch (clue.value) {
+      case 100:
+        sortedClues[100].push(clue);
+        break;
+      case 200:
+        sortedClues[200].push(clue);
+        break;
+      case 300:
+        sortedClues[300].push(clue);
+        break;
+      case 400:
+        sortedClues[400].push(clue);
+        break;
+      case 500:
+        sortedClues[500].push(clue);
+        break;
+      case 600:
+        sortedClues[600].push(clue);
+        break;
+      default:
+        break;
+    }
+  }
+
+  return selectCategoryClues(sortedClues, category);
+};
+
+const selectCategoryClues = (sortedClues, category) => {
+  const clueColumn = [];
+
+  for (let level of Object.entries(sortedClues)) {
+    clueColumn.push(level[1][Math.floor(Math.random() * level[1].length)]);
+  }
+
+  return {
+    category,
+    clueColumn,
+  };
+};
+
+export default filterCluesWithValues;
