a4c56936c681290215118f84d6b377370be06906
Kenneth Carter <kxccarter@gmail.com>
2020-11-15 18:02:33 -0600

    Successfully calls jService API 6 separate times and populates cards with questions.

--- src/components/views/GameBoardView/GameBoard_components/GameBoardColumn.js
+++ src/components/views/GameBoardView/GameBoard_components/GameBoardColumn.js
@@ -1,25 +1,70 @@
 import React, { useState, useEffect } from 'react';
+import jService from '../../../../api/jService';
+import filterCluesWithValues from '../../../../logic/getCategoryClues';
 import GameCard from './GameCard';
 
-const GameBoardColumn = ({ Cards }) => {
-    return (
-        <div className="w-1/6">
-            {Cards}
-        </div>
-    )
-}
+const GameBoardColumn = ({ categoryID }) => {
+  const [category, setCategory] = useState('');
+  const [clues, setClues] = useState([]);
+
+  console.log('Here is my categoryID: ', categoryID);
 
   useEffect(() => {
-    const { cat, clue } = getClues();
-    setCategory(cat);
-    setClues(clue);
+    getClues(categoryID);
   }, []);
 
-  const columns = clues.map((item, index) => {
-    return <GameCard Category={category} key={index} clues={item.question} />;
+  const getClues = async (categoryID) => {
+    try {
+      const { data } = await jService.get('/', {
+        params: {
+          id: categoryID,
+        },
+      });
+
+      const { category, clueColumn } = filterCluesWithValues(
+        data.clues,
+        data.title
+      );
+      setCategory(category);
+      setClues(clueColumn);
+    } catch (error) {
+      console.log('Error in getClues: ', error);
+    }
+  };
+
+  const renderedCards = clues.map((clue, index) => {
+    return (
+      <GameCard
+        key={index}
+        Type="Question"
+        Category={category}
+        Question={clue.question}
+        PointValue={clue.value}
+        Answer={clue.answer}
+      />
+    );
   });
 
-  return <div className="w-1/6">{Cards}</div>;
+  //   const Cards = [
+  //     <GameCard Type="Category" Category="Dogs" />,
+  //     <GameCard
+  //       Type="Question"
+  //       Category="dogs"
+  //       Question="This dog is spotted and often referred to in a certain movie containing the number 101"
+  //       PointValue="100"
+  //       Answer="Dalmation"
+  //     />,
+  //     <GameCard />,
+  //     <GameCard />,
+  //     <GameCard />,
+  //     <GameCard />,
+  //   ];
+
+  //   const columns = clues.map((item, index) => {
+  //     return <GameCard Category={category} key={index} clues={item.question} />;
+  //   });
+
+  return <div className="w-1/6">{renderedCards}</div>;
 };
 
 export default GameBoardColumn;
